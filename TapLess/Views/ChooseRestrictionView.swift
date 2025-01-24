52fa9d4d36f164432e166f1233e420d1aa682758
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-01-23 17:44:49 -0800

    Beautified UI

---
+++ TapLess/Views/ChooseRestrictionView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseRestrictionView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ChooseRestrictionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseRestrictionView()
+}
---
+++ TapLess/Views/customizeTypeRestrictions.swift
@@ -0,0 +1,18 @@
+//
+//  customizeTypeRestrictions.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-01-23.
+//
+
+import SwiftUI
+
+struct customizeTypeRestrictions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    customizeTypeRestrictions()
+}

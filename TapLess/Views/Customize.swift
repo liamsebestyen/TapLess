085bbcf7704f1d3869c806d88cc17e52104b6c49
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-10 07:45:42 -0800

    After lots of difficulty, I sorted the array output.

--- TapLess/Views/Customize.swift
+++ TapLess/Views/Customize.swift
@@ -301,8 +301,9 @@ struct Customize: View {
                             
                             
                             if (expandedApps.contains(appName)){
-                            
-                                ForEach(Array((createdRestrictions[appName] ?? []).enumerated()), id: \.element.id ){ index, restriction in
+                                ForEach(Array((createdRestrictions[appName] ?? [])
+                                    .sorted{$0.threshold < $1.threshold}
+                                    .enumerated()), id: \.element.id ){ index, restriction in
                                     restrictionItemView(restriction)
                                         .onTapGesture{
                                             editingRestriction = (appKey: appName, index: index, rule: restriction)

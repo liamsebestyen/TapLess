5b9a7f183e7a50889ec9eae32fc6cdc6f12e3821
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-17 06:39:34 -0800

    configure

--- TapLess/TapLessApp.swift
+++ TapLess/TapLessApp.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
+
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    FirebaseApp.configure() //configure firebase
     return true
   }
 }

4ddcd4932c36851684ac8e879e397d054c61a32c
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-16 16:51:43 -0800

    App delegate

--- TapLess/TapLessApp.swift
+++ TapLess/TapLessApp.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct TapLessApp: App {

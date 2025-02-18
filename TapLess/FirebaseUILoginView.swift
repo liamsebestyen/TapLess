f56617c51c8e5c872e67cd8c3751afc5159f6f8a
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-18 07:47:41 -0800

    New file for FirebaseLogIn

---
+++ TapLess/FirebaseUILoginView.swift
@@ -0,0 +1,19 @@
+//
+//  FirebaseUILoginView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-18.
+//
+
+import SwiftUI
+
+
+struct FirebaseUILoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirebaseUILoginView()
+}

84f0288fe39d6d24b0bf3b8eda4effc908b2d356
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-19 15:22:05 -0800

    documentation

--- TapLess/FirebaseUILoginView.swift
+++ TapLess/FirebaseUILoginView.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuthUI
 
 
 struct FirebaseUILoginView: View {
+    
+    var actionCodeSettings = ActionCodeSettings()
+    actionCodeSettings.url = URL(string: "https://example.firebasestorage.app")
+    actionCodeSettings.handleCodeInApp = true
+    actionCodeSettings.setAndroidPackageName("com.firebase.example", installIfNotAvailable: false, minimumVersion: "12")
+
+    let provider = FUIEmailAuth(authUI: FUIAuth.defaultAuthUI()!,
+                                signInMethod: FIREmailLinkAuthSignInMethod,
+                                forceSameDevice: false,
+                                allowNewEmailAccounts: true,
+                                actionCodeSetting: actionCodeSettings)
+    
+    FUIAuth.defaultAuthUI()!.handleOpen(url, sourceApplication: sourceApplication)
+    //Moving to video implementation
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }

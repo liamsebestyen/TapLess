2a9f58ad5d7763fe21909dc473dcea56972120cb
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-22 21:09:17 -0800

    New view

---
+++ TapLess/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}

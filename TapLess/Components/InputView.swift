3a236f48ee32f87ba0bb2e6de0a9af35b479f16b
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-24 08:12:09 -0800

    Login Componnent

--- TapLess/Components/InputView.swift
+++ TapLess/Components/InputView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct inputView: View {
+    @Binding var text : String
+    let title: String
+    let placeholder: String
+    var isSecureField: Bool = false
     var body: some View {
+        VStack{
+            Text(title)
+                .foregroundColor(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecureField {
+                //Password field
+                
+                SecureField(placeholder, text: $text)
+                    .font(.system(size:14))
+            
+            } else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size:14))
+                
+            }
+            Divider()
+        }
         
     }
 }
 
 #Preview {
-    inputView()
+            inputView(text: .constant(""), title: "Liam", placeholder: "liamsebestyen@uvic.ca")
 }

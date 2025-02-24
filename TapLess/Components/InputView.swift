706dfb473ceb293596f4edd84402b1c88bf51439
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-24 08:03:53 -0800

    components

---
+++ TapLess/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  inputView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-24.
+//
+
+import SwiftUI
+
+struct inputView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    inputView()
+}
---
+++ TapLess/inputView.swift
@@ -0,0 +1,18 @@
+//
+//  inputView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-24.
+//
+
+import SwiftUI
+
+struct inputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    inputView()
+}

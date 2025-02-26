087c24dc88cc373fbd565efa4b3cfdd8ede58278
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-25 20:34:46 -0800

    deleted authview controller

---
+++ TapLess/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-25.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
---
+++ TapLess/Authentication/SignInViewEmail.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewEmail.swift
+//  TapLess
+//
+//  Created by Liam Sebestyen on 2025-02-25.
+//
+
+import SwiftUI
+
+struct SignInViewEmail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInViewEmail()
+}

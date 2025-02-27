a43a668d454b04ddd1d4a7f79b04df37b81922a7
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-26 21:14:41 -0800

    Created SignInView Email

--- TapLess/Authentication/SignInViewEmail.swift
+++ TapLess/Authentication/SignInViewEmail.swift
@@ -6,13 +6,48 @@
 //
 
 import SwiftUI
+@MainActor
+final class SignInEmailViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+}
 
 struct SignInViewEmail: View {
+    @State private var viewModel = SignInEmailViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("Email ...", text: $viewModel.email)
+                .padding()
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(10)
+            
+            SecureField("Password ...", text: $viewModel.password)
+                .padding()
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(10)
+            
+            
+            Button {
+                
+            } label: {
+                Text("Sign In with Email")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In With Email")
     }
 }
 
 #Preview {
-    SignInViewEmail()
+    NavigationStack{
+        SignInViewEmail()
+    }
 }

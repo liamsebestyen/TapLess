d1e99a5724bcd9f8cf9dda9f8191d7371bad6c83
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-27 12:45:32 -0800

    Created rootview

--- TapLess/Authentication/RootView.swift
+++ TapLess/Authentication/RootView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct RootView: View {
+    @State private var showSignInView: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            NavigationStack{
+                SettingsView(showSignInView: $showSignInView)
+            }
+        }.onAppear{
+            //fetch if user authenticated
+            let authuser = try? AuthenticationManager.shared.getAuthenticatedUser()
+            self.showSignInView = authuser == nil
+            //return true if authuser is nil
+            
+        }.fullScreenCover(isPresented: $showSignInView){
+            NavigationView{
+                AuthenticationView()
+            }
+        }
     }
 }
-
 #Preview {
     RootView()
 }

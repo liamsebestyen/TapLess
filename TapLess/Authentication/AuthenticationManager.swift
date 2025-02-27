42732f2f1f9d203d8cdb62ca008800822d1118a8
Liam Sebestyen <liamsebestyen@uvic.ca>
2025-02-27 12:34:24 -0800

    Checks for authentication and signs users out

--- TapLess/Authentication/AuthenticationManager.swift
+++ TapLess/Authentication/AuthenticationManager.swift
@@ -43,6 +43,17 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: authDataResult.user)
     }
     
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser  else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return AuthDataResultModel(user: user)
+    }
     
+    func signOut() throws {
+      try  Auth.auth().signOut()
+    }
     
+
 }
